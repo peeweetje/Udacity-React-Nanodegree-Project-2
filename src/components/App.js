d79381e3a1cf15ffebc2cf1dc8a2f7b3def47ea3
Petra Vos <petravos70@gmail.com>
2017-10-13 21:47:48 +0200

    refactor: changed the route for the post_detail component

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ class App extends Component {
         <Route exact path="/:category" component={AllCategories} />
         <Route exact path="/editPost/:postId" component={EditPost} />
         <Route exact path="/editComment/:commentId" component={EditComment} />
-        <Route exact path="/:category/:postId" component={PostDetail} />
+        <Route exact path="/:category/:post_id" component={PostDetail} />
       </Switch>
     );
   }
