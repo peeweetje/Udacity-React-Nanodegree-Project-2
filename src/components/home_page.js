8a6f65f773d75a378fbf589a7e1cd90dc0445758
Petra Vos <petravos70@gmail.com>
2017-10-17 20:31:08 +0200

    feature: rendering sidebar

--- src/components/home_page.js
+++ src/components/home_page.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Timestamp from "react-timestamp";
 import Menu from "./menu";
+import SideBar from "./sideBar";
 import SortBy from "./sortBy";
 import * as actions from "../actions";
 import { List, Header, Button, Segment, Icon } from "semantic-ui-react";
@@ -30,114 +31,120 @@ class HomePage extends Component {
 
   render() {
     return (
-      <div className="header-section">
-        <div className="container">
-          <div>
-            <Header textAlign="center" color="teal" as="h1">
-              Git Talks
-            </Header>
+      <div className="page-wrapper">
+        <SideBar />
+        <div className="header-section">
+          <div className="container">
+            <div>
+              <Header textAlign="center" color="teal" as="h1">
+                Git Talks
+              </Header>
+            </div>
+            <Menu />
+            <SortBy />
           </div>
-          <Menu />
-          <SortBy />
-        </div>
 
-        {//Check if posts exist, then filter over the posts, sort the posts,
-        //and map over the posts, to display them on the HomePage
-        this.props.posts.posts &&
-          this.props.posts.posts.length > 0 &&
-          this.props.posts.posts
-            .filter(post => !post.deleted)
-            .sort((a, b) => {
-              switch (this.props.sort.sort.value) {
-                case "unpopular":
-                  return a.voteScore - b.voteScore;
-                case "oldest":
-                  return a.timestamp - b.timestamp;
-                case "newest":
-                  return b.timestamp - a.timestamp;
-                default:
-                  return b.voteScore - a.voteScore;
-              }
-            })
-            .map(post => (
-              <List key={post.id} divided relaxed>
-                <Segment color="teal" raised>
-                  <List.Item>
-                    <List.Content>
-                      <Link to={`/${post.category}/${post.id}`}>
-                        <List.Header>{post.title}</List.Header>
-                      </Link>
-                      <List.Content className="author">
-                        <Icon name="user" color="teal" size="large" />
-                        {post.author}
+          {//Check if posts exist, then filter over the posts, sort the posts,
+          //and map over the posts, to display them on the HomePage
+          this.props.posts.posts &&
+            this.props.posts.posts.length > 0 &&
+            this.props.posts.posts
+              .filter(post => !post.deleted)
+              .sort((a, b) => {
+                switch (this.props.sort.sort.value) {
+                  case "unpopular":
+                    return a.voteScore - b.voteScore;
+                  case "oldest":
+                    return a.timestamp - b.timestamp;
+                  case "newest":
+                    return b.timestamp - a.timestamp;
+                  default:
+                    return b.voteScore - a.voteScore;
+                }
+              })
+              .map(post => (
+                <List key={post.id} divided relaxed>
+                  <Segment color="teal" raised>
+                    <List.Item>
+                      <List.Content>
+                        <Link to={`/${post.category}/${post.id}`}>
+                          <List.Header>{post.title}</List.Header>
+                        </Link>
+                        <List.Content className="author">
+                          <Icon name="user" color="teal" size="large" />
+                          {post.author}
+                        </List.Content>
+                        <List.Content className="time">
+                          <Icon name="clock" color="teal" size="large" />
+                          <Timestamp
+                            time={post.timestamp / 1000}
+                            format="full"
+                          />
+                        </List.Content>
+                        <List.Content className="votes">
+                          <Icon
+                            name="thumbs up outline"
+                            color="teal"
+                            size="large"
+                            onClick={() => this.iconThumbsUp(post.id, "upVote")}
+                          />
+                          <div className="vote-score">
+                            <p className="vote-score-num">{post.voteScore}</p>
+                          </div>
+                          <Icon
+                            name="thumbs down outline"
+                            color="red"
+                            size="large"
+                            onClick={() =>
+                              this.iconThumbsDown(post.id, "downVote")}
+                          />
+                        </List.Content>
+                        <List.Content className="comments" key={post.id}>
+                          <Icon
+                            name="comment outline"
+                            color="teal"
+                            size="large"
+                          />
+                          {post.comments && post.comments.length}
+                        </List.Content>
                       </List.Content>
-                      <List.Content className="time">
-                        <Icon name="clock" color="teal" size="large" />
-                        <Timestamp time={post.timestamp / 1000} format="full" />
-                      </List.Content>
-                      <List.Content className="votes">
-                        <Icon
-                          name="thumbs up outline"
-                          color="teal"
-                          size="large"
-                          onClick={() => this.iconThumbsUp(post.id, "upVote")}
-                        />
-                        <div className="vote-score">
-                          <p className="vote-score-num">{post.voteScore}</p>
-                        </div>
-                        <Icon
-                          name="thumbs down outline"
-                          color="red"
-                          size="large"
-                          onClick={() =>
-                            this.iconThumbsDown(post.id, "downVote")}
-                        />
-                      </List.Content>
-                      <List.Content className="comments" key={post.id}>
-                        <Icon
-                          name="comment outline"
-                          color="teal"
-                          size="large"
-                        />
-                        {post.comments && post.comments.length}
-                      </List.Content>
-                    </List.Content>
-                  </List.Item>
+                    </List.Item>
 
-                  <Button
-                    onClick={() => this.deletePost(post.id)}
-                    compact
-                    basic
-                    color="red"
-                    size="tiny"
-                    floated="right"
-                  >
-                    <Icon name="trash" />
-                    Delete post
-                  </Button>
-
-                  <Link to={`/editpost/${post.id}`}>
                     <Button
+                      onClick={() => this.deletePost(post.id)}
                       compact
                       basic
-                      color="teal"
+                      color="red"
                       size="tiny"
                       floated="right"
                     >
-                      <Icon name="edit" />
-                      Edit post
+                      <Icon name="trash" />
+                      Delete post
                     </Button>
-                  </Link>
-                </Segment>
-              </List>
-            ))}
-        <div className="btn-add">
-          <Link to="/addpost">
-            <Button compact color="teal" size="large">
-              <Icon name="plus circle" />
-              Add Post
-            </Button>
-          </Link>
+
+                    <Link to={`/editpost/${post.id}`}>
+                      <Button
+                        compact
+                        basic
+                        color="teal"
+                        size="tiny"
+                        floated="right"
+                      >
+                        <Icon name="edit" />
+                        Edit post
+                      </Button>
+                    </Link>
+                  </Segment>
+                </List>
+              ))}
+          <div className="btn-add">
+            <Link to="/addpost">
+              <Button compact color="teal" size="large">
+                <Icon name="plus circle" />
+                Add Post
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     );
