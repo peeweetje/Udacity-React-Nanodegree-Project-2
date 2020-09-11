ec5ab925a21ced8c48f6483fe557cd19963134cc
peeweetje <petravos70@gmail.com>
2020-09-11 13:52:19 +0200

    Upgrade semantic-ui-react

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import HomePage from "./home-page/home_page";
-import AllCategories from "./all-categories/all_categories";
-import PostDetail from "./post-details/post_detail";
-import EditPost from "./edit-post/edit_post";
-import EditComment from "./edit-comment/edit_comment";
-import AddPost from "./add-post/add_post";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './home-page/home_page';
+import AllCategories from './all-categories/all_categories';
+import PostDetail from './post-details/post_detail';
+import EditPost from './edit-post/edit_post';
+import EditComment from './edit-comment/edit_comment';
+import AddPost from './add-post/add_post';
 
-import "../App.scss";
+import '../App.scss';
 
 const App = () => {
   return (
     <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/addPost" component={AddPost} />>
-      <Route exact path="/:category/" component={AllCategories} />
-      <Route exact path="/editPost/:postId" component={EditPost} />
-      <Route exact path="/editComment/:commentId" component={EditComment} />
-      <Route exact path="/:category/:post_id" component={PostDetail} />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/addPost' component={AddPost} />
+      <Route exact path='/:category/' component={AllCategories} />
+      <Route exact path='/editPost/:postId' component={EditPost} />
+      <Route exact path='/editComment/:commentId' component={EditComment} />
+      <Route exact path='/:category/:post_id' component={PostDetail} />
     </Switch>
   );
 };
