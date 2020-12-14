296ae239cbdf67ede209fb4dab5d9a05570f1774
peeweetje <petravos70@gmail.com>
2020-12-14 20:54:41 +0100

    add theming

---
+++ src/themes.js
@@ -0,0 +1,24 @@
+ export const theme = {
+    colors: {
+      primary: `#00b5ad`,
+    }
+  }
+
+  export const darkTheme = {
+    backgroundColor: "#333333",
+    textColor: "#000000",
+    primary: "#77ccdd"
+  }
+  
+  export const lightTheme = {
+    backgroundColor: "#e0e0e0",
+    textColor: "#ffffff",
+    primary: "#55aacc"
+  }
+  
+  export const partyTheme = {
+    backgroundColor: "#eeff00",
+    textColor: "#ff88cc",
+    primary: "#aa00ee"
+  }
+  
