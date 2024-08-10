7f959ca4f558ab9abaa085770909b5cc45b96a64
PEEWEE\petra <petravos70@gmail.com>
2024-08-10 17:21:28 +0200

    Refactor the component into a functional component.

--- src/components/add-post/add_post.js
+++ src/components/add-post/add_post.js
@@ -1,137 +1,110 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Form, Header, Icon } from "semantic-ui-react";
-import { fetchAddPost } from "../../redux/actions";
-import Menu from "../menu/menu";
-import SideBar from "../sidebar/sideBar";
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Form, Header, Icon } from 'semantic-ui-react';
+import { fetchAddPost } from '../../redux/actions';
+import SideBar from '../sidebar/sideBar';
 import { v1 as uuidv1 } from 'uuid';
 
-import './add-post.scss'
+import './add-post.scss';
 
-//Variable to hold the values of the dropdown menu for the post category.
 const options = [
-  { key: 1, text: "React", value: "react" },
-  { key: 2, text: "Redux", value: "redux" },
-  { key: 3, text: "Udacity", value: "udacity" },
-  { key: 4, text: "Javascript", value: "javascript" },
+  { key: 1, text: 'React', value: 'react' },
+  { key: 2, text: 'Redux', value: 'redux' },
+  { key: 3, text: 'Udacity', value: 'udacity' },
+  { key: 4, text: 'Javascript', value: 'javascript' },
 ];
 
-class AddPost extends Component {
-  //State to keep track of the post details.
-  state = {
-    postCategory: "react",
-    postTitle: "",
-    postAuthor: "",
-    postContent: "",
+const AddPost = ({ history }) => {
+  const [postCategory, setPostCategory] = useState('react');
+  const [postTitle, setPostTitle] = useState('');
+  const [postAuthor, setPostAuthor] = useState('');
+  const [postContent, setPostContent] = useState('');
+  const dispatch = useDispatch();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'postTitle') setPostTitle(value);
+    if (name === 'postAuthor') setPostAuthor(value);
+    if (name === 'postContent') setPostContent(value);
   };
 
-  //Handles input in the form fields
-  handleInputChange = e => {
-    const target = e.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  //Sets the value of the chosen category in the dropdown menu
-  setPostCategory = (e, data) => {
-    this.setState({ postCategory: data.value });
-  };
-
-  //Submits the post detail data.
-  handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const data = {
       id: uuidv1(),
       timestamp: Date.now(),
-      title: this.state.postTitle,
-      body: this.state.postContent,
-      author: this.state.postAuthor,
-      category: this.state.postCategory,
+      title: postTitle,
+      body: postContent,
+      author: postAuthor,
+      category: postCategory,
       deleted: false,
       voteScore: 1,
     };
-    //Dispatches fetchAddPost action with the added post detail data.
-    this.props.fetchAddPost(data);
-    //Redirects to the HomePage after adding a new post.
-    this.props.history.push("/");
+    dispatch(fetchAddPost(data));
+    history.push('/');
   };
 
-  render() {
-    return (
-      <div className="page-wrapper">
-        <SideBar />
-        <div className="add-post-form ">
-          <Header
-            className="add-post-header"
-            textAlign="center"
-            color="teal"
-            as="h1"
+  return (
+    <div className='page-wrapper'>
+      <SideBar />
+      <div className='add-post-form'>
+        <Header
+          className='add-post-header'
+          textAlign='center'
+          color='teal'
+          as='h1'
+        >
+          <Icon name='edit' /> Add New Post
+        </Header>
+        <Form className='addpost-form' onSubmit={handleSubmit}>
+          <Form.Dropdown
+            label='Category'
+            options={options}
+            value={postCategory}
+            onChange={(e, data) => setPostCategory(data.value)}
+            selection
+          />
+          <Form.Input
+            required
+            label='Title'
+            name='postTitle'
+            value={postTitle}
+            onChange={handleInputChange}
+            label='Post Title'
+            placeholder='Post Title'
+          />
+          <Form.Input
+            required
+            label='Author'
+            name='postAuthor'
+            value={postAuthor}
+            onChange={handleInputChange}
+            label='Author'
+            placeholder='Author'
+          />
+          <Form.TextArea
+            required
+            label='Content'
+            name='postContent'
+            value={postContent}
+            onChange={handleInputChange}
+            placeholder='Post Content'
+            rows={6}
+          />
+
+          <Form.Button
+            name='form-button-control-public'
+            color='teal'
+            compact
+            size='large'
           >
+            <Icon name='plus circle' />
             Add Post
-          </Header>
-          <div className="add-post-menu">
-            <Menu />
-          </div>
-
-          <Form className="addpost-form" onSubmit={this.handleSubmit}>
-            <Form.Select
-              required
-              name="postCategory"
-              placeholder="Category"
-              label="Choose Category"
-              value={this.state.postCategory}
-              onChange={this.setPostCategory}
-              options={options}
-            />
-
-            <Form.Input
-              required
-              name="postTitle"
-              id="post-title"
-              value={this.state.postTitle}
-              onChange={this.handleInputChange}
-              label="Post Title"
-              placeholder="Post Title"
-            />
-            <Form.Input
-              required
-              name="postAuthor"
-              value={this.state.postAuthor}
-              onChange={this.handleInputChange}
-              label="Author"
-              placeholder="Author"
-            />
-
-            <Form.TextArea
-              required
-              name="postContent"
-              value={this.state.postContent}
-              onChange={this.handleInputChange}
-              label="Post Content"
-              placeholder="Post Content"
-              rows={6}
-            />
-            <Form.Button
-              name="form-button-control-public"
-              color="teal"
-              compact
-              size="large"
-            >
-              <Icon name="plus circle" />
-              Add Post
-            </Form.Button>
-          </Form>
-        </div>
+          </Form.Button>
+        </Form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-//Add the fetchAddPost action directly to the connect method, so a
-//mapDispatchToProps method isn't needed.
-
-export default connect(null, { fetchAddPost })(AddPost);
+export default AddPost;
