bf4c61ba66a273ef5b0a4d3c4bc167fb8bb3ad4c
PEEWEE\petra <petravos70@gmail.com>
2024-08-11 10:05:53 +0200

    Refactor component into a functional component.

--- src/components/edit-post/edit_post.js
+++ src/components/edit-post/edit_post.js
@@ -1,144 +1,147 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { fetchEditPost, fetchSinglePost } from "../../redux/actions";
-import Menu from "../menu/menu";
-import SideBar from "../sidebar/sideBar";
-import { Form, Header, Icon } from "semantic-ui-react";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { fetchEditPost, fetchSinglePost } from '../../redux/actions';
+import SideBar from '../sidebar/sideBar';
+import { Form, Header, Icon } from 'semantic-ui-react';
 
 const options = [
-  { key: 1, text: "React", value: "react" },
-  { key: 2, text: "Redux", value: "redux" },
-  { key: 3, text: "Udacity", value: "udacity" },
-  { key: 4, text: "Javascript", value: "javascript" },
+  { key: 1, text: 'React', value: 'react' },
+  { key: 2, text: 'Redux', value: 'redux' },
+  { key: 3, text: 'Udacity', value: 'udacity' },
+  { key: 4, text: 'Javascript', value: 'javascript' },
 ];
 
-class EditPost extends Component {
-  state = {
-    id: "",
-    postCategory: "",
-    postTitle: "",
-    postAuthor: "",
-    postContent: "",
-  };
+const EditPost = ({
+  match,
+  fetchSinglePost,
+  fetchEditPost,
+  posts,
+  history,
+}) => {
+  const [post, setPost] = useState({
+    id: '',
+    postCategory: '',
+    postTitle: '',
+    postAuthor: '',
+    postContent: '',
+  });
 
-  componentDidMount() {
-    const { postId } = this.props.match.params;
-    //Fetches post matching a postId, then uses the data from the post to set the state,
-    //and prepopulate the edit post form with this data.
-    this.props.fetchSinglePost(postId).then(() => {
-      const { id, title, author, body, category } = this.props.posts.posts[0];
-      this.setState({
-        id: id,
-        postTitle: title,
-        postAuthor: author,
-        postContent: body,
-        postCategory: category,
-      });
+  useEffect(() => {
+    const { postId } = match.params;
+    fetchSinglePost(postId).then(() => {
+      const fetchedPost = posts.posts.find((post) => post.id === postId);
+      if (fetchedPost) {
+        const { id, title, author, body, category } = fetchedPost;
+        setPost({
+          id,
+          postTitle: title,
+          postAuthor: author,
+          postContent: body,
+          postCategory: category,
+        });
+      }
     });
-  }
+  }, [match.params, fetchSinglePost]);
 
-  handleInputChange = e => {
-    const target = e.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setPost((prevPost) => ({
+      ...prevPost,
       [name]: value,
-    });
+    }));
   };
 
-  setPostCategory = (e, data) => {
-    this.setState({ postCategory: data.value });
-    console.log(this.state);
+  const setPostCategory = (e, data) => {
+    setPost((prevPost) => ({
+      ...prevPost,
+      postCategory: data.value,
+    }));
   };
 
-  handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { id, postTitle, postCategory, postContent, postAuthor } = this.state;
+    const { id, postTitle, postCategory, postContent, postAuthor } = post;
     const data = {
-      id: id,
+      id,
       title: postTitle,
       body: postContent,
       author: postAuthor,
       category: postCategory,
     };
-    this.props.fetchEditPost(data, data.id);
-    this.props.history.push("/");
+    fetchEditPost(data, data.id);
+    history.push('/');
   };
 
-  render() {
-    return (
-      <div className="page-wrapper">
-        <SideBar />
-
-        <div className="editpost-header">
-          <Header textAlign="center" color="teal" as="h1">
-            Edit Post
-          </Header>
-        </div>
-
-        <div className="edit-post-menu">
-          <Menu />
-        </div>
-        <div className="add-post-form">
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Select
-              required
-              name="postCategory"
-              placeholder="Category"
-              label="Choose Category"
-              value={this.state.postCategory}
-              onChange={this.setPostCategory}
-              options={options}
-            />
-
-            <Form.Input
-              required
-              name="postTitle"
-              id="post-title"
-              value={this.state.postTitle}
-              onChange={this.handleInputChange}
-              label="Post Title"
-              placeholder="Post Title"
-            />
-            <Form.Input
-              required
-              name="postAuthor"
-              value={this.state.postAuthor}
-              onChange={this.handleInputChange}
-              label="Author"
-              placeholder="Author"
-            />
-
-            <Form.TextArea
-              required
-              name="postContent"
-              value={this.state.postContent}
-              onChange={this.handleInputChange}
-              label="Post Content"
-              placeholder="Post Content"
-              rows={6}
-            />
-            <Form.Button
-              name="form-button-control-public"
-              color="teal"
-              compact
-              size="large"
-            >
-              <Icon name="edit" />
-              Edit Post
-            </Form.Button>
-          </Form>
-        </div>
+  return (
+    <div className='page-wrapper'>
+      <SideBar />
+      <div className='editpost-header'>
+        <Header color='teal' as='h1' textAlign='center'>
+          <Icon name='edit' />
+          Edit Post
+        </Header>
       </div>
-    );
-  }
-}
+      <div className='add-post-form'>
+        <Form onSubmit={handleSubmit}>
+          <Form.Dropdown
+            label='Category'
+            options={options}
+            value={post.postCategory}
+            onChange={setPostCategory}
+            selection
+          />
+          <Form.Input
+            required
+            label='Title'
+            name='postTitle'
+            value={post.postTitle}
+            onChange={handleInputChange}
+            label='Post Title'
+            placeholder='Post Title'
+          />
+          <Form.Input
+            required
+            label='Author'
+            name='postAuthor'
+            value={post.postAuthor}
+            onChange={handleInputChange}
+            onChange={handleInputChange}
+            label='Author'
+            placeholder='Author'
+          />
+          <Form.TextArea
+            required
+            label='Content'
+            name='postContent'
+            value={post.postContent}
+            onChange={handleInputChange}
+            placeholder='Post Content'
+            rows={6}
+          />
 
-const mapStateToProps = ({ posts }) => ({
-  posts,
+          <Form.Button
+            type='submit'
+            name='form-button-control-public'
+            color='teal'
+            compact
+            size='large'
+          >
+            <Icon name='edit' />
+            Edit Post
+          </Form.Button>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  posts: state.posts,
 });
 
-export default connect(mapStateToProps, { fetchEditPost, fetchSinglePost })(
-  EditPost
-);
+const mapDispatchToProps = {
+  fetchSinglePost,
+  fetchEditPost,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditPost);
