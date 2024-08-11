a581fd6ced2b626fa12763ddab195cc1736b8735
PEEWEE\petra <petravos70@gmail.com>
2024-08-11 11:07:46 +0200

    Refactor component into a functional component.

--- src/components/edit-comment/edit_comment.js
+++ src/components/edit-comment/edit_comment.js
@@ -1,106 +1,92 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { fetchEditComment, fetchComment } from "../../redux/actions";
-import Menu from "../menu/menu";
-import SideBar from "../sidebar/sideBar";
-import { Form, Header, Icon } from "semantic-ui-react";
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchEditComment, fetchComment } from '../../redux/actions';
+import Menu from '../menu/menu';
+import SideBar from '../sidebar/sideBar';
+import { Form, Header, Icon } from 'semantic-ui-react';
 
-class EditComment extends Component {
-  state = {
-    commentAuthor: "",
-    commentContent: "",
+const EditComment = ({ match, history }) => {
+  const [commentAuthor, setCommentAuthor] = useState('');
+  const [commentContent, setCommentContent] = useState('');
+  const dispatch = useDispatch();
+  const receiveComment = useSelector((state) => state.receiveComment);
+
+  useEffect(() => {
+    dispatch(fetchComment(match.params.commentId));
+  }, [dispatch, match.params.commentId]);
+
+  useEffect(() => {
+    if (receiveComment) {
+      const { author, body } = receiveComment;
+      setCommentAuthor(author);
+      setCommentContent(body);
+    }
+  }, [receiveComment]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'commentAuthor') {
+      setCommentAuthor(value);
+    } else if (name === 'commentContent') {
+      setCommentContent(value);
+    }
   };
 
-  componentDidMount() {
-    //Get the comment by commentId, then set the state using the data
-    //from the comment, and prepopulate the form with this data.
-    this.props.fetchComment(this.props.match.params.commentId).then(() => {
-      const { author, body } = this.props.receiveComment;
-      this.setState({
-        commentAuthor: author,
-        commentContent: body,
-      });
-    });
-  }
-
-  handleInputChange = e => {
-    const target = e.target;
-    const value = target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  //Submits input data from form fields
-  handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { commentContent, commentAuthor } = this.state;
     const data = {
-      id: this.props.receiveComment.id,
+      id: receiveComment.id,
       body: commentContent,
       author: commentAuthor,
     };
-    //Dispatched editComment action with data from form
-    this.props.fetchEditComment(data, data.id);
-    //Redirects back to previous page.
-    this.props.history.goBack();
+    dispatch(fetchEditComment(data, data.id));
+    history.goBack();
   };
 
-  render() {
-    return (
-      <div className="page-wrapper">
-        <SideBar />
-        <div className="add-post-form ">
-          <Header
-            className="editcomment-header"
-            textAlign="center"
-            color="teal"
-            as="h1"
+  return (
+    <div className='page-wrapper'>
+      <SideBar />
+      <div className='add-post-form'>
+        <Header
+          className='editcomment-header'
+          textAlign='center'
+          color='teal'
+          as='h1'
+        >
+          Edit Comment
+        </Header>
+
+        <Menu />
+
+        <Form onSubmit={handleSubmit}>
+          <Form.Input
+            required
+            name='commentAuthor'
+            value={commentAuthor}
+            onChange={handleInputChange}
+            label='Author'
+          />
+          <Form.Input
+            required
+            name='commentContent'
+            value={commentContent}
+            onChange={handleInputChange}
+            label='Comment Content'
+            rows={6}
+          />
+          <Form.Button
+            name='form-button-control-public'
+            color='teal'
+            compact
+            size='large'
           >
-            Edit Comment
-          </Header>
-
-          <Menu />
-
-          <Form onSubmit={this.handleSubmit}>
-            <Form.Input
-              required
-              name="commentAuthor"
-              value={this.state.commentAuthor}
-              onChange={this.handleInputChange}
-              label="Author"
-            />
-
-            <Form.TextArea
-              required
-              name="commentContent"
-              value={this.state.commentContent}
-              onChange={this.handleInputChange}
-              label="Comment Content"
-              rows={6}
-            />
-            <Form.Button
-              name="form-button-control-public"
-              color="teal"
-              compact
-              size="large"
-            >
-              <Icon name="edit" />
-              Edit comment
-            </Form.Button>
-          </Form>
-        </div>
+            <Icon name='edit' />
+            Edit comment
+          </Form.Button>
+        </Form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-const mapStateToProps = ({ receiveComment }) => ({
-  receiveComment,
-});
-
-export default connect(mapStateToProps, { fetchEditComment, fetchComment })(
-  EditComment
-);
+export default EditComment;
