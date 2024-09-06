ef79fb08e2ae9d664589b06c4a828604a085b8f6
PEEWEE\petra <petravos70@gmail.com>
2024-09-06 11:58:01 +0200

    refactor component into a functional component.

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,76 +1,58 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { fetchCategories, fetchPostsCategory } from "../../redux/actions";
-import SortBy from "../sort-by/sortBy";
-import { Grid, Button, Responsive } from "semantic-ui-react";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchCategories, fetchPostsCategory } from '../../redux/actions';
+import SortBy from '../sort-by/sortBy';
+import { Grid, Button, Responsive } from 'semantic-ui-react';
 
-class Menu extends Component {
-  //Get all the categories, to display in the Menu.
-  componentDidMount() {
-    this.props.fetchCategories();
-  }
+const Menu = () => {
+  const dispatch = useDispatch();
+  const receiveCategories = useSelector((state) => state.receiveCategories);
 
-  //Dispatches action to get the posts for a category, when clicking on a Menu Button.
-  getPostsByCategory = category => {
-    this.props.fetchPostsCategory(category);
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
+  const getPostsByCategory = (category) => {
+    dispatch(fetchPostsCategory(category));
   };
 
-  render() {
-    const { receiveCategories } = this.props;
-    return (
-      <div className="categories">
-        <Responsive as={Grid} columns={6} minWidth={768}>
-          <Grid.Column>
-            <Link to="/">
-              <Button
-                className="menu-btn"
-                size="tiny"
-                compact
-                basic
-                color="teal"
-              >
-                All
-              </Button>
-            </Link>
-          </Grid.Column>
-          {receiveCategories.length > 0 &&
-            receiveCategories.map(category => (
-              <Grid.Column key={category.path}>
-                <Link to={`/${category.name}`}>
-                  <Button
-                    className="menu-btn"
-                    onClick={() => this.getPostsByCategory(category.name)}
-                    size="tiny"
-                    compact
-                    basic
-                    color="teal"
-                  >
-                    {category.name}
-                  </Button>
-                </Link>
-              </Grid.Column>
-            ))}
-          <Grid.Column>
-            <SortBy />
-          </Grid.Column>
-        </Responsive>
-        <Responsive as={Grid} columns={1} maxWidth={767}>
+  return (
+    <div className='categories'>
+      <Responsive as={Grid} columns={6} minWidth={768}>
+        <Grid.Column>
+          <Link to='/'>
+            <Button className='menu-btn' size='tiny' compact basic color='teal'>
+              All
+            </Button>
+          </Link>
+        </Grid.Column>
+        {receiveCategories.length > 0 &&
+          receiveCategories.map((category) => (
+            <Grid.Column key={category.path}>
+              <Link to={`/${category.name}`}>
+                <Button
+                  className='menu-btn'
+                  onClick={() => getPostsByCategory(category.name)}
+                  size='tiny'
+                  compact
+                  basic
+                  color='teal'
+                >
+                  {category.name}
+                </Button>
+              </Link>
+            </Grid.Column>
+          ))}
+        <Grid.Column>
           <SortBy />
-        </Responsive>
-      </div>
-    );
-  }
-}
+        </Grid.Column>
+      </Responsive>
+      <Responsive as={Grid} columns={1} maxWidth={767}>
+        <SortBy />
+      </Responsive>
+    </div>
+  );
+};
 
-const mapStateToProps = ({ receiveCategories }) => ({
-  receiveCategories,
-});
-
-//Pass actions directly into connect method, so mapDispatchToProps function
-//isn't needed, and less code is needed.
-
-export default connect(mapStateToProps, {
-  fetchCategories,
-  fetchPostsCategory,
-})(Menu);
+export default Menu;
