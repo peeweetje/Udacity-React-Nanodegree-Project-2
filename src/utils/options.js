817443368255b1482fde5d609022bc4d680d7ee7
PEEWEE\petra <petravos70@gmail.com>
2024-09-14 10:54:59 +0200

    create a new file for te options.

---
+++ src/utils/options.js
@@ -0,0 +1,13 @@
+export const options = [
+  { key: 1, text: 'React', value: 'react' },
+  { key: 2, text: 'Redux', value: 'redux' },
+  { key: 3, text: 'Udacity', value: 'udacity' },
+  { key: 4, text: 'Javascript', value: 'javascript' },
+];
+
+export const valueOptions = [
+  { value: 'popular', text: 'Popular' },
+  { value: 'unpopular', text: 'Unpopular' },
+  { value: 'oldest', text: 'Oldest' },
+  { value: 'newest', text: 'Newest' },
+];
