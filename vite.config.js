8cbc13c09a612e1d420f42219e11c055742f4206
PEEWEE\petra <petravos70@gmail.com>
2024-09-22 12:00:22 +0200

    Set up configuration for the styling solution.

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -10,5 +11,10 @@ export default defineConfig(() => {
       outDir: 'build',
     },
     plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
   };
 });
