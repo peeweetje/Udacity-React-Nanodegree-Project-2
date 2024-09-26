9955745be281fddf3c776199f15f44408c8bbbcb
PEEWEE\petra <petravos70@gmail.com>
2024-09-26 20:48:19 +0200

    Refactor the component by using the card component from shadcn/ui. W.I.P

--- src/components/home-page/home_page.jsx
+++ src/components/home-page/home_page.jsx
@@ -1,180 +1,122 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import Timestamp from 'react-timestamp';
-import Menu from '../menu/menu';
-import SideBar from '../sidebar/sideBar';
-import * as actions from '../../redux/actions';
-import { sortPosts } from '../../utils/sortPosts';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import Timestamp from 'react-timestamp'
+import * as actions from '../../redux/actions'
+import { sortPosts } from '../../utils/sortPosts'
+
+import { Button } from "@/components/ui/button"
 import {
-  List,
-  Header,
-  Button,
-  Segment,
-  Icon,
-  Responsive,
-} from 'semantic-ui-react';
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { ThumbsUp, ThumbsDown, MessageSquare, Trash2, Edit, PlusCircle, User, Clock } from 'lucide-react'
 
-import './home-page.scss';
+import Menu from '../menu/menu'
+import SideBar from '../sidebar/sideBar'
 
-const HomePage = () => {
-  const dispatch = useDispatch();
-  const posts = useSelector((state) => state.posts.posts);
-  const sort = useSelector((state) => state.sort.sort);
+export default function HomePage() {
+  const dispatch = useDispatch()
+  const posts = useSelector((state) => state.posts.posts)
+  const sort = useSelector((state) => state.sort.sort)
 
   useEffect(() => {
-    // Fetch posts to display on Home Page
-    dispatch(actions.fetchPosts());
-  }, [dispatch]);
+    dispatch(actions.fetchPosts())
+  }, [dispatch])
 
   const deletePost = (postId) => {
-    dispatch(actions.fetchDeletePost(postId));
-  };
+    dispatch(actions.fetchDeletePost(postId))
+  }
 
   const iconThumbsUp = (postId) => {
-    dispatch(actions.fetchVotePost(postId, 'upVote'));
-  };
+    dispatch(actions.fetchVotePost(postId, 'upVote'))
+  }
 
   const iconThumbsDown = (postId) => {
-    dispatch(actions.fetchVotePost(postId, 'downVote'));
-  };
+    dispatch(actions.fetchVotePost(postId, 'downVote'))
+  }
 
   return (
-    <div className='page-wrapper'>
+    <div className="flex min-h-screen bg-background">
       <SideBar />
-      <div className='content-wrapper'>
-        <div className='container'>
-          <div className='header-section'>
-            <Header textAlign='center' color='teal' as='h1'>
-              Git Talks
-            </Header>
+      <div className="flex-1 p-8">
+        <div className="container mx-auto">
+          <div className="text-center mb-8">
+            <h1 className="text-4xl font-bold text-primary">Git Talks</h1>
           </div>
           <Menu />
         </div>
 
-        <div className='post-container'>
-          {/* Check if posts exist, then filter, sort, and map over the posts to display them on the HomePage */}
+        <div className="grid gap-6 mt-8">
           {posts &&
             posts.length > 0 &&
             sortPosts(
               posts.filter((post) => !post.deleted),
               sort.value
             ).map((post) => (
-              <List key={post.id} divided relaxed>
-                <Segment color='teal' raised>
-                  <List.Item>
-                    <List.Content>
-                      <Link to={`/${post.category}/${post.id}`}>
-                        <List.Header>{post.title}</List.Header>
-                      </Link>
-                      <List.Content className='author'>
-                        <Icon name='user' color='teal' size='large' />
-                        {post.author}
-                      </List.Content>
-                      <List.Content className='time'>
-                        <Icon name='clock' color='teal' size='large' />
-                        <Timestamp
-                          options={{ twentyFourHour: true }}
-                          date={post.timestamp / 1000}
-                        />
-                      </List.Content>
-                      <List.Content className='votes'>
-                        <Icon
-                          name='thumbs up outline'
-                          color='teal'
-                          size='large'
-                          onClick={() => iconThumbsUp(post.id)}
-                        />
-                        <div className='vote-score'>
-                          <p className='vote-score-num'>{post.voteScore}</p>
-                        </div>
-                        <Icon
-                          name='thumbs down outline'
-                          color='red'
-                          size='large'
-                          onClick={() => iconThumbsDown(post.id)}
-                        />
-                      </List.Content>
-                      <List.Content className='comments' key={post.id}>
-                        <Icon
-                          name='comment outline'
-                          color='teal'
-                          size='large'
-                        />
-                        {post.comments && post.comments.length}
-                      </List.Content>
-                    </List.Content>
-                  </List.Item>
-                  <div className='post-btn-wrapper'>
-                    <Responsive
-                      as={Button}
-                      onClick={() => deletePost(post.id)}
-                      compact
-                      basic
-                      color='red'
-                      size='tiny'
-                      floated='right'
-                      maxWidth={400}
-                    >
-                      <Icon name='trash' />
-                      Delete
-                    </Responsive>
-                    <Responsive
-                      as={Button}
-                      onClick={() => deletePost(post.id)}
-                      compact
-                      basic
-                      color='red'
-                      size='tiny'
-                      floated='right'
-                      minWidth={401}
-                    >
-                      <Icon name='trash' />
-                      Delete post
-                    </Responsive>
-
-                    <Link to={`/editpost/${post.id}`}>
-                      <Responsive
-                        as={Button}
-                        compact
-                        basic
-                        color='teal'
-                        size='tiny'
-                        floated='left'
-                        maxWidth={400}
-                      >
-                        <Icon name='edit' />
-                        Edit
-                      </Responsive>
-                      <Responsive
-                        as={Button}
-                        compact
-                        basic
-                        color='teal'
-                        size='tiny'
-                        floated='right'
-                        minWidth={401}
-                      >
-                        <Icon name='edit' />
-                        Edit post
-                      </Responsive>
+              <Card key={post.id}>
+                <CardHeader>
+                  <CardTitle>
+                    <Link to={`/${post.category}/${post.id}`} className="hover:underline">
+                      {post.title}
                     </Link>
+                  </CardTitle>
+                  <CardDescription>
+                    <div className="flex items-center space-x-2">
+                      <User className="h-4 w-4" />
+                      <span>{post.author}</span>
+                    </div>
+                    <div className="flex items-center space-x-2 mt-1">
+                      <Clock className="h-4 w-4" />
+                      <Timestamp date={post.timestamp / 1000} options={{ twentyFourHour: true }} />
+                    </div>
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex items-center space-x-4">
+                    <Button variant="ghost" size="sm" onClick={() => iconThumbsUp(post.id)}>
+                      <ThumbsUp className="h-4 w-4 mr-2" />
+                    </Button>
+                    <span className="font-bold">{post.voteScore}</span>
+                    <Button variant="ghost" size="sm" onClick={() => iconThumbsDown(post.id)}>
+                      <ThumbsDown className="h-4 w-4 mr-2" />
+                    </Button>
+                    <div className="flex items-center">
+                      <MessageSquare className="h-4 w-4 mr-2" />
+                      <span>{post.comments && post.comments.length}</span>
+                    </div>
                   </div>
-                </Segment>
-              </List>
+                </CardContent>
+                <CardFooter className="justify-between">
+                  <Button variant="outline" size="sm" asChild>
+                    <Link to={`/editpost/${post.id}`}>
+                      <Edit className="h-4 w-4 mr-2" />
+                      <span className="hidden sm:inline">Edit post</span>
+                      <span className="sm:hidden">Edit</span>
+                    </Link>
+                  </Button>
+                  <Button variant="destructive" size="sm" onClick={() => deletePost(post.id)}>
+                    <Trash2 className="h-4 w-4 mr-2" />
+                    <span className="hidden sm:inline">Delete post</span>
+                    <span className="sm:hidden">Delete</span>
+                  </Button>
+                </CardFooter>
+              </Card>
             ))}
-          <div className='btn-add'>
-            <Link to='/addpost'>
-              <Button compact color='teal' size='large'>
-                <Icon name='plus circle' />
-                Add Post
-              </Button>
+        </div>
+        <div className="mt-8 text-center">
+          <Button asChild>
+            <Link to="/addpost">
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add Post
             </Link>
-          </div>
+          </Button>
         </div>
       </div>
     </div>
-  );
-};
-
-export default HomePage;
+  )
+}
