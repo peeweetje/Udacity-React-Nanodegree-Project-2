80fa223834299a2560bf55cdcee76101c518c10d
PEEWEE\petra <petravos70@gmail.com>
2024-10-08 19:56:36 +0200

    Remove packages no longer needed.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
