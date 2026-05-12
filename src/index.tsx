03a1799831b8400d9afb752c8b550413ff50546b
peeweetje <petravos70@gmail.com>
2026-05-12 09:12:02 +0200

    Store the theme in the locale storage.

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import '../i18n';
 import Loading from './components/loading/loading';
 
+// Restore theme from localStorage before React renders to prevent flash
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
+
 const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
