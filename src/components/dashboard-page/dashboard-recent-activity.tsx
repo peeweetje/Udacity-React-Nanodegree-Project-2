9c661794afa2b6f5ce8c5b387ebcc9edea09eb96
peeweetje <petravos70@gmail.com>
2026-05-18 10:11:32 +0200

    Update the dark theme.

--- src/components/dashboard-page/dashboard-recent-activity.tsx
+++ src/components/dashboard-page/dashboard-recent-activity.tsx
@@ -53,8 +53,8 @@ const DashboardRecentActivity = ({ activities }: DashboardRecentActivityProps) =
                   <span
                     className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${
                       activity.status === 'active'
-                        ? 'bg-green-100 text-green-800'
-                        : 'bg-gray-100 text-gray-600'
+                        ? 'bg-green-100 text-green-800 dark:bg-teal-900/80 dark:text-teal-200'
+                        : 'bg-gray-100 text-gray-600 dark:bg-gray-600 dark:text-gray-200'
                     }`}
                   >
                     {activity.status === 'active'
