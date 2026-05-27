37bd4fae209cf1013c1fa1e13d041034a655bb8a
peeweetje <petravos70@gmail.com>
2026-05-27 10:37:40 +0200

    Make the select smoother.

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -93,7 +93,9 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        'relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-gray-800 text-neutral-950 dark:text-neutral-50 shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+        'relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-gray-800 text-neutral-950 dark:text-neutral-50 shadow-md',
+        'data-[state=open]:animate-[select-slide-up_200ms_ease-out]',
+        'data-[state=closed]:animate-[select-slide-down_150ms_ease-in]',
         position === 'popper' &&
           'data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1',
         className
