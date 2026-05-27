3185875f1f7cda964719fbc83032a9465ad58375
peeweetje <petravos70@gmail.com>
2026-05-27 10:36:03 +0200

    make sure the sort by is also animated.

--- src/components/sort-by/sortBy.tsx
+++ src/components/sort-by/sortBy.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import gsap from 'gsap';
 import { changeSortAction } from '../../redux/actions';
 import {
   Select,
@@ -25,13 +26,33 @@ const SortBy = () => {
   const dispatch = useDispatch();
   const value = useSelector((state: RootState) => state.sort.value);
   const valueOptions = createValueOptions(t);
+  const animationsEnabled = useSelector((state: any) => state.animations?.enabled ?? true);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const el = containerRef.current;
+    if (!el) return;
+
+    if (!animationsEnabled) {
+      gsap.set(el, { y: 0, opacity: 1 });
+      return;
+    }
+    gsap.set(el, { y: -20, opacity: 0 });
+    gsap.to(el, {
+      y: 0,
+      opacity: 1,
+      duration: 0.5,
+      ease: 'power3.out',
+      delay: 0.3,
+    });
+  }, [animationsEnabled]);
 
   const handleChange = (newValue: string) => {
     dispatch(changeSortAction({ value: newValue }));
   };
 
   return (
-    <div className='w-[100px]'>
+    <div ref={containerRef} className='sort-by-container w-[100px]'>
       <Select value={value} onValueChange={handleChange}>
         <SelectTrigger className='w-full'>
           <SelectValue placeholder={t('common.sort-by')} />
